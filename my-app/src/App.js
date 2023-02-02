25c8e97740c36cebe6a85fd43ace7eb934f1abbf
ToniDodd <121724318+ToniDodd@users.noreply.github.com>
2023-02-02 12:08:27 -0800

    Rebuilt app, added react, bootstrap, axios and transferred html and css

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Weather from "./Weather";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
